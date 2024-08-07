3b04898311c2b1f6307190dae3ec0147ffae54f5
HaroonMasihDevDen <haroon.masih@devden.io>
2024-08-07 17:23:00 +0500

    show categories on client side from server api call

---
+++ ecommerce_frontend/src/api/category.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { CATEGORIES_API } from '../config/api';
+
+export const getCategories = async () => {
+    try {
+      const response = await axios.get(CATEGORIES_API, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      console.log("Categories response:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching categories:", error.response?.data);
+      throw new Error(error.response?.statusText || 'Network response was not ok for categories');
+    }
+};
+
--- ecommerce_frontend/src/config/api.js
+++ ecommerce_frontend/src/config/api.js
@@ -12,7 +12,10 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const getApiUrl = (endpoint) => `${API_BASE_URL}${endpoint}`;
 
-// API routes
+// auth.js
 export const LOGIN_API = getApiUrl('/login');
 export const LOGOUT_API = getApiUrl('/logout');
 export const VALIDATE_TOKEN_API = getApiUrl('/validate_token');
+
+// category.js
+export const CATEGORIES_API = getApiUrl('/categories');
